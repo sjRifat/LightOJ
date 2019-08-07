a088a0946d85437ae34f48a385a9073e8b3e4596
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-08-07 21:43:38 +0600

    Create 1000 - Greetings from LightOJ.cpp

---
+++ 1000 - Greetings from LightOJ.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ 
+int main ()
+{
+    int a,b,x,n,i;
+ 
+    scanf ("%d",&n);
+    for (i=1; i<=n; i++)
+    {
+        scanf ("%d%d", &a, &b);
+        printf ("Case %d: %d\n", i, a+b);
+    }
+ 
+    return 0;
+ 
+}
