8252dcab63f8a557a2e9910ed994e0fb810d38e8
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-10-18 19:28:11 +0600

    Update 1005 - Rooks.cpp

--- 1005 - Rooks.cpp
+++ 1005 - Rooks.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define		pb              push_back
-#define		PB              pop_back
-#define		nn              "\n"
-#define		all(p)          p.begin(),p.end()
-#define		zz(v)           (ll)v.size()
-#define		sp              ' '
-#define 	S(a)            scanf("%lld",&a)
-#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
-#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
-#define		gcd(a,b)        __gcd(a,b)
-#define		lcm(a,b)        (a*b)/gcd(a,b)
-#define		pi              acos(-1.0)
-#define		mod             1000003
-typedef		long long       ll;
-typedef		vector<ll>      vll;
+#define     pb              push_back
+#define     PB              pop_back
+#define     nn              "\n"
+#define     all(p)          p.begin(),p.end()
+#define     zz(v)           (ll)v.size()
+#define     sp              ' '
+#define     S(a)            scanf("%lld",&a)
+#define     SS(a,b)         scanf("%lld %lld",&a,&b)
+#define     SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define     gcd(a,b)        __gcd(a,b)
+#define     lcm(a,b)        (a*b)/gcd(a,b)
+#define     pi              acos(-1.0)
+#define     mod             1000003
+typedef     long long       ll;
+typedef     vector<ll>      vll;
 
 ll fac(ll a,ll b){
     ll m=1;
@@ -29,11 +29,11 @@ int main(){
     while(t--){
         SS(n,r);
         x=n-r;
-        xx=fac(x+1,n);
-        y=fac(1,r);
-        z=fac(x+1,n);
-        printf("Case %lld: %lld\n",++c,(xx/y)*z);
+        y=fac(x+1,n);
+        z=fac(1,r);
+        printf("Case %lld: %lld\n",++c,(y/z)*y);
     }
 
     return 0;
 }
+
